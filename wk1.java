8b3fcfe007a1d4ed5aa63f4c6c78c471b1279e97
yl22011 <92855412+yl22011@users.noreply.github.com>
2022-01-25 11:58:53 +0800

    Add files via upload

---
+++ wk1.java
@@ -0,0 +1,24 @@
+class Main {
+    public static void main(String[] args) {
+       System.out.println("Hello I am Ming Le");
+       
+       
+       String modulecode= "CS1009";
+       switch(modulecode)
+       {
+        case "CS1009":
+          System.out.println("Object-Oriented Programming");
+          break;
+        default:
+          System.out.println("Unknown Module");
+          break;
+        
+          
+       }
+  
+       for(int x =101; x>66; x= x-2)
+       {
+         System.out.println("value of x :" + x);
+       }
+    }
+  } 
